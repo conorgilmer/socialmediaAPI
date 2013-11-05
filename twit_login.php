2f194d0622f6f785e7769738d98321ddced3e140
Conor Gilmer <conor.gilmer@gmail.com>
2013-11-05 19:04:13 +0000

    twit

---
+++ twit_login.php
@@ -0,0 +1,26 @@
+<?php
+require("twitteroauth.php");
+require 'inc/app_tokens_twitter.inc.php';
+session_start();
+
+$twitteroauth = new TwitterOAuth($consumer_key, $consumer_secret);
+// Requesting authentication tokens, the parameter is the URL we will be redirected to
+$request_token = $twitteroauth->getRequestToken('localhost/socialmediaAPI/private.php');
+
+// Saving them into the session
+
+$_SESSION['oauth_token'] = $request_token['oauth_token'];
+$_SESSION['oauth_token_secret'] = $request_token['oauth_token_secret'];
+//$sname = $request_token['screen_name'];
+
+// If everything goes well..
+if ($twitteroauth->http_code == 200) {
+    // Let's generate the URL and redirect
+        $_SESSION["loggedIn"]=1;
+    $url = $twitteroauth->getAuthorizeURL($request_token['oauth_token']);
+    header('Location: ' . $url);
+} else {
+    // It's a bad idea to kill the script, but we've got to know when there's an error.
+    die('Something wrong happened.');
+}
+?>
