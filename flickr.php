84ecf8d1e61783d0cf833387c8761e1d3cf2f520
Conor Gilmer <conor.gilmer@gmail.com>
2015-12-01 16:32:25 +0000

    add pinterest

--- flickr.php
+++ flickr.php
@@ -10,17 +10,22 @@
       <hr-->
       
        <div class="row">
-        <div class="col-lg-12">
+        <div class="col-lg-6">
 <h1> Flickr </h1>
 <p>Some pics posted on Flickr, using the widget</p>
+
+<!-- flickr  widget for me -->
+<iframe src="https://www.flickr.com/photos/conorgilmer/15317103436/in/photostream/player/" width="350" height="350" frameborder="0" allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>
         </div>
+
+        <div class="col-lg-6">
+<h1> Pinterest </h1>
+<!-- pinterest widget for me -->
+
+     <a data-pin-do="embedUser" href="https://www.pinterest.com/conorgilmer/"data-pin-scale-width="80" data-pin-scale-height="200" data-pin-board-width="400">    Visit Conor's profile on Pinterest.</a><!-- Please call pinit.js only once per page --><script type="text/javascript" async src="//assets.pinterest.com/js/pinit.js"></script>
+
+        </div>
+
       </div><!-- /.row -->
       
-
- <div class="row text-center">
-
-<iframe src="https://www.flickr.com/photos/conorgilmer/15317103436/in/photostream/player/" width="350" height="350" frameborder="0" allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>
-
-      </div><!-- /.row -->
-          
      <?php include('footer.php'); ?>
