007122d740f75ef6cd78152dafaffc6353b2950a
Conor Gilmer <conor.gilmer@gmail.com>
2015-12-04 23:36:09 +0000

    unset

--- logout.php
+++ logout.php
@@ -1,6 +1,19 @@
 <?php
 session_start();
+
+//unset session login data variables
 unset($_SESSION['github_data']);
+unset($_SESSION['instagram_data']);
+unset($_SESSION['login_data']);
+unset($_SESSION['yahoo_data']);
+unset($_SESSION['linkedin_data']);
+unset($_SESSION['google_data']);
+unset($_SESSION['facebook_data']);
+unset($_SESSION['twitter_data']);
+
+// just in case destroy session
 session_destroy();
+
+// redirect to index page
 header("Location: index.php");
 ?>
