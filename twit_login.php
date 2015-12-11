714a10c88067da763b77919e38276215d3422828
Conor Gilmer <conor.gilmer@gmail.com>
2015-12-11 16:05:37 +0000

    check for no connection

--- twit_login.php
+++ twit_login.php
@@ -7,14 +7,20 @@ session_start();
 
 $twitteroauth = new TwitterOAuth($consumer_key, $consumer_secret);
 // Requesting authentication tokens, the parameter is the URL we will be redirected to
-$request_token = $twitteroauth->getRequestToken('localhost/socialmediaAPI/private.php');
+//$request_token = $twitteroauth->getRequestToken('http://www.conorgilmer.com/socialmediaAPI/private.php');
+$request_token = $twitteroauth->getRequestToken('http://localhost/socialmediaAPI/private.php');
 
 // Saving them into the session
-
 $_SESSION['oauth_token'] = $request_token['oauth_token'];
 $_SESSION['oauth_token_secret'] = $request_token['oauth_token_secret'];
-$_SESSION['twitter_results'] = $request_token['screen_name'];
+//$_SESSION['twitter_results'] = $request_token['screen_name'];
 $_SESSION["route"] = "Twitter";
+
+
+// catch when there is no connection i.e. twitter or curl blocked by ISP
+if ($twitteroauth->http_code == 0) {
+	die("NO TWITTER CONNECTION - either CURL blocked or Twitter blocked");
+}
 
 // If everything goes well..
 if ($twitteroauth->http_code == 200) {
