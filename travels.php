71723b9bb5214a0db73c97663585a077f795b416
Conor Gilmer <conor.gilmer@gmail.com>
2016-08-17 12:26:32 +0100

    added my travels

---
+++ travels.php
@@ -0,0 +1,23 @@
+<?php include('header.php'); ?>
+
+<div class="container">
+
+        <!--div class="jumbotron hero-spacer">
+        <h1>My Web presences!</h1>
+        <p>A composite website showing my social media accounts!</p>
+      </div>
+      
+      <hr-->
+      
+       <div class="row">
+        <div class="col-lg-12">
+           <h3>My Travels</h3>
+        </div>
+      </div><!-- /.row -->
+      
+          
+             <div class="row text-center">
+<img src="http://chart.apis.google.com/chart?cht=map:fixed=-70,-180,80,180&chs=450x300&chf=bg,s,336699&chco=d0d0d0,cc0000&chd=s:9999999999999999999&chld=BE|DK|EE|FI|FR|DE|GR|IS|IE|IT|SE|ES|SK|PL|NO|AT|CZ|US|CU" width="450" height="300" ><br/>visited 19 states (8.44%)<br/><a href="http://douweosinga.com/projects/visited?region=world">Create your own visited map of The World</a> or <a href="https://play.google.com/store/apps/details?id=com.triposo.droidguide.amsterdam&hl=en">Amsterdam travel guide for Android</a>
+
+      </div><!-- /.row -->
+     <?php include('footer.php'); ?>
