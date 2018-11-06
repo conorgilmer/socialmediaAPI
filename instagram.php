4f98b6ebfa57d0918d5044194984adf05d39fe38
Conor Gilmer <conor.gilmer@gmail.com>
2018-11-06 00:53:59 +0000

    updated Instagram page to InstaWidget

--- instagram.php
+++ instagram.php
@@ -12,14 +12,19 @@
        <div class="row">
         <div class="col-lg-12">
 <h1> My Instagram </h1>
-<p>Some pics posted on Instagram, using the widget</p>
+<p>Some Instagram, using the InstaWidget</p>
         </div>
       </div><!-- /.row -->
  
 
 <div class="row text-center">
 
-<!-- INSTANSIVE WIDGET --><script src="//instansive.com/widget/js/instansive.js"></script><iframe src="//instansive.com/widgets/f9f7fdc1f0a86bbf54bfa4fe069a5a3a07c36637.html" id="instansive_f9f7fdc1f0" name="instansive_f9f7fdc1f0"  scrolling="no" allowtransparency="true" class="instansive-widget" style="width: 100%; border: 0; overflow: hidden;"></iframe>
+<!-- INSTANSIVE WIDGET -->
+<!--<script src="//instansive.com/widget/js/instansive.js"></script><iframe src="//instansive.com/widgets/f9f7fdc1f0a86bbf54bfa4fe069a5a3a07c36637.html" id="instansive_f9f7fdc1f0" name="instansive_f9f7fdc1f0"  scrolling="no" allowtransparency="true" class="instansive-widget" style="width: 100%; border: 0; overflow: hidden;"></iframe>
+-->
+<!-- InstaWidget -->
+<a href="https://instawidget.net/v/user/conorgilmer" id="link-a70d50f67dfadbf8215e390a4238a9bf5eb641fe35f2b3de83d2bcf2bd9587b5">@conorgilmer</a>
+<script src="https://instawidget.net/js/instawidget.js?u=a70d50f67dfadbf8215e390a4238a9bf5eb641fe35f2b3de83d2bcf2bd9587b5&width=300px"></script>
 
       </div><!-- /.row -->
 
