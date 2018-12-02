22c940f44233085f48247e559c74f1bae4e385e6
Conor Gilmer <conor.gilmer@gmail.com>
2018-12-02 12:56:45 +0000

    clean up index page

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
               <p>Login Natively!</p>
               <p><a href="native_login.php" class="btn btn-primary">Native Login</a></p>
               
-               <p>u: guest p: letmein</p>
+               <!--p>u: guest p: letmein</p-->
             </div>
           </div>
         </div>
@@ -67,7 +67,55 @@
             </div>
           </div>
         </div>
-
-
       </div><!-- /.row -->
+<!-- -->
+
+
+      <div class="row text-center">
+      <div class="col-lg-3 col-md-6 hero-feature">
+          <div class="thumbnail">
+            <!--img src="http://placehold.it/800x500" alt=""-->
+            <div class="caption">
+              <h3>GitHub Login</h3>
+              <p>Login with your GitHub Account</p>
+              <p><a href="github_login.php" class="btn btn-primary">GitHub Login</a></p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 hero-feature">
+          <div class="thumbnail">
+            <!--img src="http://placehold.it/800x500" alt=""-->
+            <div class="caption">
+              <h3>LinkedIn Login</h3>
+              <p>Login with your LinkedIn Account</p>
+              <p><a href="linkedin_login.php" class="btn btn-primary">LinkedIn Login</a> </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 hero-feature">
+          <div class="thumbnail">
+            <!--img src="http://placehold.it/800x500" alt=""-->
+            <div class="caption">
+              <h3>Yahoo Login</h3>
+              <p>Login with your Yahoo Account!</p>
+              <p><a href="yahoo_login.php" class="btn btn-primary">Yahoo Login</a></p>
+            </div>
+          </div>
+        </div>
+ <div class="col-lg-3 col-md-6 hero-feature">
+          <div class="thumbnail">
+            <!--img src="http://placehold.it/800x500" alt=""-->
+            <div class="caption">
+              <h3>OpenID Login</h3>
+              <p>Login with your OpenID Account!</p>
+              <p><a href="openid_login.php" class="btn btn-primary">OpenID Login</a></p>
+            </div>
+          </div>
+        </div>
+
+
+
+</div><!-- /.row -->
      <?php include('footer.php'); ?>
